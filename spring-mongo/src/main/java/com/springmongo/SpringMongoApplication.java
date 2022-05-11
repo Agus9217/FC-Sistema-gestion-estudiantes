7ba896aa2070d459d54d4651ccf32c2e6a58160d
agustinibarrola <justinibarrola92@gmail.com>
2022-05-10 23:24:46 -0300

    Mongo

--- spring-mongo/src/main/java/com/springmongo/SpringMongoApplication.java
+++ spring-mongo/src/main/java/com/springmongo/SpringMongoApplication.java
@@ -3,10 +3,13 @@ package com.springmongo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
-import java.time.ZonedDateTime;
 import java.util.List;
 
 @SpringBootApplication
@@ -15,17 +18,27 @@ public class SpringMongoApplication {
 	public static void main(String[] args) {
 		ApplicationContext context = SpringApplication.run(SpringMongoApplication.class, args);
 		StudentRepository repository = context.getBean(StudentRepository.class);
+		MongoTemplate mongoTemplate = context.getBean(MongoTemplate.class);
+
 		Address address = new Address("Argentina", "Buenos Aires", "CP1716");
+
+		String email = "justinibarrola92@gmail.com";
 		Student student = new Student(
 				null,
 				"Agustin",
 				"Ibarrola",
-				"justinibarrola92@gmail.com",
+				email,
 				Gender.MALE,
 				address,
-				List.of("Computer Science", "Maths"),
+				List.of("Computer Science", "Maths", "Justin"),
 				BigDecimal.TEN,
 				LocalDateTime.now());
+
+
+		Query query = new Query();
+		query.addCriteria(Criteria.where("email").is(email) );
+
+
 
 		repository.insert(student);
 	}
